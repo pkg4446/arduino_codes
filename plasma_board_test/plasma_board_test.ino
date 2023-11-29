980be35656b98ce07bc1372b2f8417eac115c7d1
pkg4446 <pkg4446@gmail.com>
2023-11-29 10:50:20 +0900

    231129

---
+++ plasma_board_test/plasma_board_test.ino
@@ -0,0 +1,44 @@
+#define UPDATE_INTERVAL 1000L
+
+const int8_t Relay[6] = {13,12,14,16,17,23};
+const int8_t led[3]   = {27,32,33};
+
+unsigned long prevUpdateTime = 0L;
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(Relay[0], OUTPUT);
+  pinMode(Relay[1], OUTPUT);
+  pinMode(Relay[2], OUTPUT);
+  pinMode(Relay[3], OUTPUT);
+  pinMode(Relay[4], OUTPUT);
+  pinMode(Relay[5], OUTPUT);
+
+  pinMode(led[0], OUTPUT);
+  pinMode(led[1], OUTPUT);
+  pinMode(led[2], OUTPUT);
+
+  relayOnOff(0);
+  relayOnOff(1);
+  relayOnOff(2);
+  relayOnOff(3);
+  relayOnOff(4);
+  relayOnOff(5);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  for (int i = 0; i < 3; i++) {
+    digitalWrite(led[i], HIGH);
+    delay(1000);
+    digitalWrite(led[i], LOW);
+    delay(1000);
+  }
+}
+
+void relayOnOff(int8_t pinNumber) {
+  digitalWrite(Relay[pinNumber], HIGH);
+  delay(1000);
+  digitalWrite(Relay[pinNumber], LOW);
+  delay(1000);
+}
