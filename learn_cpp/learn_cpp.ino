f8c01de887d96520c0924ed11913eb7799ddfab1
pkg4446 <pkg4446@gmail.com>
2024-02-06 13:49:36 +0900

    240206

--- learn_cpp/learn_cpp.ino
+++ learn_cpp/learn_cpp.ino
@@ -39,6 +39,8 @@ uint32_t  calendar    = 1;
 uint8_t   hour_count  = 6;
 uint8_t   season      = 0; // month = rand(12);
 
+uint8_t   aggro_point = 0;
+
 unsigned long time_clock  = 0UL;
 bool          time_stop   = false;
 uint32_t    one_hour_sec  = ONE_HOUR;
