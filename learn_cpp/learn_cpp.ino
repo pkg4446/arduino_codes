2ace41cafbc6db944fb9b07ace0f8363f858b425
pkg4446 <pkg4446@gmail.com>
2024-02-07 11:21:48 +0900

    240207

--- learn_cpp/learn_cpp.ino
+++ learn_cpp/learn_cpp.ino
@@ -228,8 +228,8 @@ void setup() {
     scene_number = COMMAND_MAIN;
   }
 
-  pregnant_baby(path_assist(),path_avatar(),random(2));
-  dir_move(path_assist(),"/test");
+  //pregnant_baby(path_assist(),path_avatar(),random(2));
+  //dir_move(path_assist(),"/test");
 
   time_clock = millis();
   //display_newday(&calendar,info_class,stat_class,mens_class,current_class);
