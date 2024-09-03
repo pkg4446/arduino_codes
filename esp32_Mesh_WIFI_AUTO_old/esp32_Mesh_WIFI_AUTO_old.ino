3d0939a65d3312b6cd7ed0f2b5d7a7fbb68e2877
pkg4446 <pkg4446@gmail.com>
2024-09-03 16:28:57 +0900

    240903

--- esp32_Mesh_WIFI_AUTO_old/esp32_Mesh_WIFI_AUTO_old.ino
+++ esp32_Mesh_WIFI_AUTO_old/esp32_Mesh_WIFI_AUTO_old.ino
@@ -5,11 +5,11 @@
 HardwareSerial rootDvice(2);
 
 #include "EEPROM.h"
-#define   EEPROM_SIZE 16
+#define   EEPROM_SIZE 24
 
 /******************EEPROM******************/
-const uint8_t eep_ssid[EEPROM_SIZE] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-const uint8_t eep_pass[EEPROM_SIZE] = {16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31};
+const uint8_t eep_ssid[EEPROM_SIZE] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23};
+const uint8_t eep_pass[EEPROM_SIZE] = {24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47};
 /******************EEPROM******************/
 char ssid[EEPROM_SIZE];
 char password[EEPROM_SIZE]; //#234567!
