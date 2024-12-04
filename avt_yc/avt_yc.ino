cd94e6acad6bed515a3f9bddc01c879d4ae0d7bf
Kwanggu <pkg4446@gmail.com>
2024-12-04 20:26:54 +0900

    241204

--- avt_yc/avt_yc.ino
+++ avt_yc/avt_yc.ino
@@ -9,7 +9,7 @@
 #include <Adafruit_SHT31.h>
 #include "uart_print.h"
 
-String firmwareVersion = "0.0.1";
+String firmwareVersion = "0.0.2";
 
 #define TCAADDR                   0x70
 #define TOTAL_TEMPERATURE_SENSOR  5
@@ -171,9 +171,13 @@ void system_control(unsigned long millisec){
     if(!manual_mode){
       for (uint8_t index = 0; index < TOTAL_TEMPERATURE_SENSOR; index++){
         if(heat_use && temperature_sensor_tm[index]<temperature_goal[index]-temperature_gap){
-          heater_state[index] = true;
-          heater_worker += 1;
-          heater_working[index] += 1;
+          if(temperature_sensor_tm[index]<1 && temperature_goal[index]<temperature_sensor_ic[index]){
+            heater_state[index] = false;
+          }else{
+            heater_state[index] = true;
+            heater_worker += 1;
+            heater_working[index] += 1;
+          }
         }else heater_state[index] = false;
         digitalWrite(MOSFET[index], heater_state[index]);
       }
